e46df6726d66f123ec2bac6431d317b059246d36
张旭童 <mcxtzhang@163.com>
2017-11-23 18:22:35 +0800

    【feature】 在滑动 和 记录log中 加入延迟

--- swipe_log.py
+++ swipe_log.py
@@ -2,6 +2,8 @@
 import commands
 import sys
 
+import time
+
 import LogHandler
 
 # print "begin start activity...."
@@ -17,16 +19,19 @@ scheme = sys.argv[1]
 
 logHandler = LogHandler.LogHandler(scheme)
 # 后期可配置 循环次数
-for num in range(0, 20):
+for num in range(0, 10):
     print "begin swipe...."
     # 滑动策略后期可能会变
-    commands.getstatusoutput("adb shell input swipe 800 800 200 200")
+    commands.getstatusoutput("adb shell input swipe 800 800 800 500")
     # print status
     # print output
     fileName = "logs/log_%s_%s" % (ZUtils.getTimeSuffix(), num)
+    time.sleep(3)
+
     print "begin dumpsys and save to file:%s" % fileName
     (status, output) = commands.getstatusoutput(
         "adb shell dumpsys gfxinfo com.dianping.v1 reset framestats > %s" % fileName)
+
     # 处理文件
     if num == 0:
         logHandler.handleLog("%s" % fileName)
