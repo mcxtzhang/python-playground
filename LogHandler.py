741239650ca208bd5f25bc0715aab538e93cef7a
张旭童 <mcxtzhang@163.com>
2017-11-23 15:43:28 +0800

    【feature】 输出file 第一行加入scheme

--- LogHandler.py
+++ LogHandler.py
@@ -17,26 +17,19 @@ class LogHandler:
         self.scheme = scheme
         self.__fileStringMap = {}
 
+    # 第一次处理log
     def handleLog(self, fileName):
         # print "handleLog with %s \n" % fileName
         file_object = open(fileName)
         try:
             fileString = file_object.read()
-            # remove useless log
-            beginIndex = fileString.index(KEY_BEGIN) + len(KEY_BEGIN)
-            endIndex = fileString.index(KEY_END)
-            fileString = fileString[beginIndex:endIndex].strip()
-
-            # list = []
+            fileString = self._removeUselessInfo(fileString)
 
             indexComplete = fileString.find(KEY_FRAME_COMPLETE)
 
             while indexComplete > -1:
 
                 profileIndex = fileString.index(KEY);
-                # activity信息
-                activityInfo = fileString[0: profileIndex].strip()
-                temp = activityInfo
 
                 completeIndex = fileString.index(KEY_FRAME_COMPLETE) + len(KEY_FRAME_COMPLETE)
                 secondProfileIndex = fileString.index(KEY, profileIndex + len(KEY))
@@ -47,9 +40,16 @@ class LogHandler:
                 # print "len(KEY):%s"%len(KEY)
                 # 没有fps数据的不写入文件
                 if completeIndex + 1 != secondProfileIndex:
+                    # 1 scheme信息
+                    temp = self.scheme + "\n"
+
+                    # 2 activity信息
+                    activityInfo = fileString[0: profileIndex].strip()
+                    temp = temp + activityInfo
+
+                    # 3 fps info
                     temp = temp + fileString[completeIndex: secondProfileIndex]
                     self.__fileStringMap[activityInfo] = temp
-                    # list.append(temp)
 
                 # 截取 继续循环
                 fileString = fileString[secondProfileIndex + len(KEY):]
@@ -60,34 +60,28 @@ class LogHandler:
             file_object.close()
 
     def mergeLog(self, fileName):
-
         # print ("mergeLog with %s" % fileName)
-        # print "handleLog with %s \n" % fileName
         file_object = open(fileName)
         try:
             fileString = file_object.read()
-            # remove useless log
-            beginIndex = fileString.index(KEY_BEGIN) + len(KEY_BEGIN)
-            endIndex = fileString.index(KEY_END)
-            fileString = fileString[beginIndex:endIndex].strip()
+            fileString = self._removeUselessInfo(fileString)
 
             indexComplete = fileString.find(KEY_FRAME_COMPLETE)
 
             while indexComplete > -1:
-
                 profileIndex = fileString.index(KEY);
-                # activity信息
-                activityInfo = fileString[0: profileIndex].strip()
-                #temp = activityInfo
 
                 completeIndex = fileString.index(KEY_FRAME_COMPLETE) + len(KEY_FRAME_COMPLETE)
                 secondProfileIndex = fileString.index(KEY, profileIndex + len(KEY))
 
                 # 没有fps数据的不写入文件
                 if completeIndex + 1 != secondProfileIndex:
+                    # 2 activity信息  和上面的差别 activityinfo 仅仅用来定位
+                    activityInfo = fileString[0: profileIndex].strip()
+
+                    # 3 fps info
                     temp = fileString[completeIndex: secondProfileIndex]
                     self.__fileStringMap[activityInfo] = self.__fileStringMap[activityInfo] + temp
-                    # list.append(temp)
 
                 # 截取 继续循环
                 fileString = fileString[secondProfileIndex + len(KEY):]
@@ -97,6 +91,7 @@ class LogHandler:
         finally:
             file_object.close()
 
+    # 一次task完成，输出n个file，每个file内是一个Activity的 fps info
     def outputFile(self):
         # 遍历字典列表
         i = 1
@@ -106,3 +101,10 @@ class LogHandler:
             file_output.write(values)
             file_output.close()
             i = i + 1
+
+    def _removeUselessInfo(self, fileString):
+        # remove useless log
+        beginIndex = fileString.index(KEY_BEGIN) + len(KEY_BEGIN)
+        endIndex = fileString.index(KEY_END)
+        fileString = fileString[beginIndex:endIndex].strip()
+        return fileString
